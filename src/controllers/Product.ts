07680f489b6b90cb3c7cd70eecdcae343e903a63
Ana Laura <analauraalves25@gmail.com>
2022-05-26 21:12:53 -0300

    Feat: Creates products controller to create new product

--- src/controllers/Product.ts
+++ src/controllers/Product.ts
@@ -15,4 +15,17 @@ export default class ProductController {
       return next(e);
     }
   };
+
+  public create = async (req: Request, res: Response, next: NextFunction):
+  Promise<Response | void> => {
+    try {
+      const { name, amount } = req.body;
+
+      const result = await this.service.create(name, amount);
+
+      return res.status(201).json(result);
+    } catch (e) {
+      return next(e);
+    }
+  };
 }
