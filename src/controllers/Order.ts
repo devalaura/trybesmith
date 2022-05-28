8d73f5b9bb98fd2666ca19431f6f729f2aa4ef71
Ana Laura <analauraalves25@gmail.com>
2022-05-28 00:34:52 -0300

    Feat: Creates order controller to get all orders

---
+++ src/controllers/Order.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+
+import OrderService from '../services/Order';
+
+export default class OrderController {
+  constructor(public service = new OrderService()) {}
+
+  public getAll = async (_req: Request, res: Response, next: NextFunction):
+  Promise<Response | void> => {
+    try {
+      const orders = await this.service.getAll();
+
+      return res.status(201).json(orders);
+    } catch (e) {
+      return next(e);
+    }
+  };
+}
