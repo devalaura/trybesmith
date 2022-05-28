965203ba4bc2926807eda3f37b1cb18f8eb278f0
Ana Laura <analauraalves25@gmail.com>
2022-05-28 00:39:42 -0300

    Feat: Creates order service and interface to get all orders

---
+++ src/services/Order.ts
@@ -0,0 +1,16 @@
+import OrderModel from '../models/Order';
+import connection from '../models/connection';
+import Order from '../interfaces/Order';
+
+export default class OrderService {
+  public model: OrderModel;
+
+  constructor() {
+    this.model = new OrderModel(connection);
+  }
+
+  public getAll = async (): Promise<Order> => {
+    const orders = await this.model.getAll();
+    return orders;
+  };
+}
